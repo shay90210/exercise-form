15a89438d04aab3b7558de18b01dbe2926d0d3f2
Shannon Bentley <bentley.shannon@yahoo.com>
2022-11-08 18:38:16 -0600

    created exercise form.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.scss';
+import ExerciseForm from '../src/components/ExerciseForm';
 
 function App() {
   return (
     <div className="App">
-
+      <ExerciseForm />
     </div>
   );
 }
